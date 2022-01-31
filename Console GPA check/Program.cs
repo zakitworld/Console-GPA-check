f19fe890cd27fe214c4d59ad6c0660faddc7b542
SHAGGY1 Abdul PC <abdulrazakjibrilla138@gmail.com>
2022-01-30 17:17:48 -0800

    Add project files.

---
+++ Console GPA check/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Console_GPA_check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double gpa;
+            Console.WriteLine("Enter your gpa:");
+            gpa = Convert.ToDouble(Console.ReadLine());
+            if (gpa >= 2.0)
+            {
+                if (gpa >= 3.9)
+                {
+                    Console.WriteLine("Deans Honour List.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("The GPA is below the graduation requirement");
+                Console.WriteLine("See your academic advisor");
+            }
+        }
+    }
+}
